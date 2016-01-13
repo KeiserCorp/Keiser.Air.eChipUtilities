e97f841015bb8391993bbd83caaa18817cbe845a
Zach Cardoza <bayssmekanique@live.com>
2016-01-13 14:54:07 -0800

    Adds standalong build step

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var source = require('vinyl-source-stream');
+var streamify = require('gulp-streamify');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+var mainjs = './src/main.js';
+
+gulp.task('bundle', function () {
+	browserify(mainjs, {standalone: 'keu'}).bundle()
+	.pipe(source(mainjs))
+	.pipe(rename({
+			dirname : "/",
+			basename : "keu"
+		}))
+	.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('bundle-min', function () {
+	browserify(mainjs, {standalone: 'keu'}).bundle()
+	.pipe(source(mainjs))
+	.pipe(streamify(uglify()))
+	.pipe(rename({
+			dirname : "/",
+			basename : "keu",
+			suffix : ".min"
+		}))
+	.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', function () {
+	gulp.start('bundle', 'bundle-min');
+});
+
+gulp.task('watch', function () {
+	gulp.watch('./src/*.js', ['bundle', 'bundle-min']);
+});
--- keu.js
+++
@@ -1,8 +0,0 @@
-module.exports = function () {
-	'use strict';
-	var keu = {};
-	keu.machines = require('./src/machine_definitions');
-
-	return keu;
-}
-();
